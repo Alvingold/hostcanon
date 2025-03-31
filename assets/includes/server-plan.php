d372741e2392ce37f7f609a77015cb1218bde8e2
jace-jacobs <jacobidjereh@gmail.com>
2025-03-31 13:14:58 +0100

    modified:   assets/css/dedicated-hosting.css 	modified:   assets/css/style.css 	modified:   assets/includes/server-plan.php 	modified:   dedicated-hosting.php 	modified:   tests.php

--- assets/includes/server-plan.php
+++ assets/includes/server-plan.php
@@ -11,6 +11,12 @@
     <!-- Bootstrap Icons CDN -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css"> -->
     <style>
+
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: content-box;
+        }
         body{
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             margin: 0;
